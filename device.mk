16cf97b6fd36131d105ef16e463984d1d97e5dd7
Jamie Falconer <mrfluffyoven@gmail.com>
2025-10-17 08:12:00 +0100

    Add some touch things

--- device.mk
+++ device.mk
@@ -30,6 +30,20 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl.recovery \
     android.hardware.health@2.1-service
 
+# Add these lines to ensure touchscreen modules are included
+PRODUCT_PACKAGES += \
+    android.hardware.input.classifier@1.0-service \
+    android.hardware.input.processor@1.0-service
+
+# Include touch firmware in recovery
+PRODUCT_COPY_FILES += \
+    vendor/firmware/touchscreen/hx83112f_BOE_FW.bin:recovery/root/vendor/firmware/hx83112f_BOE_FW.bin \
+    vendor/firmware/touchscreen/hx83112f_BOE_mp.bin:recovery/root/vendor/firmware/hx83112f_BOE_mp.bin \
+    vendor/firmware/touchscreen/hx83112f_TXD_FW.bin:recovery/root/vendor/firmware/hx83112f_TXD_FW.bin \
+    vendor/firmware/touchscreen/hx83112f_TXD_mp.bin:recovery/root/vendor/firmware/hx83112f_TXD_mp.bin \
+    vendor/firmware/touchscreen/chipone_firmware.bin:recovery/root/vendor/firmware/chipone_firmware.bin \
+    vendor/firmware/touchscreen/chipone_limit.bin:recovery/root/vendor/firmware/chipone_limit.bin
+
 # QCOM
 PRODUCT_PACKAGES += \
     qcom_decrypt \
